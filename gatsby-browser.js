30eb4eb043002cba53230e264c90cbae82ac7376
Radosław Łuckoś <radoslaw.luckos@angrynerds.co>
2022-09-23 15:29:48 +0200

    Delete gatsby-browser.js

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-const React = require('react');
-const { MainTemplate } = require('./src/components/MainTemplate');
-
-exports.wrapPageElement = ({ element, props }) => {
-    return <MainTemplate {...props}>{element}</MainTemplate>
-}
